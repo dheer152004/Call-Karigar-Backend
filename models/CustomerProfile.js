5719d4b01be78cdb32378cda2d5995b793a0d136
dheer152004 <dheer152004@gmail.com>
2025-08-05 21:57:55 +0530

    first commit

---
+++ models/CustomerProfile.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
+const { addressSchema } = require('./Addresses');
+
+const customerProfileSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    default: uuidv4
+  },
+  userId: {
+    type: String,
+    ref: 'User',
+    required: true,
+    unique: true
+  },
+  address: [
+    addressSchema
+
+  ],
+  phoneNumber: {
+    type: String,
+    required: true
+  },
+  preferences: {
+    language: {
+      type: String,
+      default: 'en'
+    },
+    notifications: {
+      type: Boolean,
+      default: true
+    }
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('CustomerProfile', customerProfileSchema);
